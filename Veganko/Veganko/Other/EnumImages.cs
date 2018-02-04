87921cd938090cafad9ad65f87adbeb2a365db9f
Bostjan Kamnik <bkamnik.mmt-b2014@fh-salzburg.ac.at>
2018-02-04 11:56:54 +0100

    NewProductView ProductType binding fixed

--- Veganko/Veganko/Other/EnumImages.cs
+++ Veganko/Veganko/Other/EnumImages.cs
@@ -18,12 +18,12 @@ namespace Veganko
         //};
         public static Dictionary<ProductClassifier, string> ProductClassifierImages => new Dictionary<ProductClassifier, string>
         {
-            { ProductClassifier.Vegeterijansko, "vegan.png" },
-            { ProductClassifier.Vegansko, "vegan.png" },
-            { ProductClassifier.Pesketarijansko, "vegan.png" },
-            { ProductClassifier.GlutenFree, "vegan.png" },
-            { ProductClassifier.RawVegan, "vegan.png" },
-            { ProductClassifier.CrueltyFree, "vegan.png" },
+            { ProductClassifier.Vegeterijansko, "ico_vegan.png" },
+            { ProductClassifier.Vegansko, "ico_vegan.png" },
+            { ProductClassifier.Pesketarijansko, "ico_vegan.png" },
+            { ProductClassifier.GlutenFree, "ico_vegan.png" },
+            { ProductClassifier.RawVegan, "ico_vegan.png" },
+            { ProductClassifier.CrueltyFree, "ico_vegan.png" },
         };
 
         public static Dictionary<ProductType, string> ProductTypeImages => new Dictionary<ProductType, string>
