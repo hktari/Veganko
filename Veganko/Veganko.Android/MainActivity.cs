4c16575711168186ed455abd10e5030114a2b9b9
Bostjan Kamnik <bkamnik.mmt-b2014@fh-salzburg.ac.at>
2018-02-09 19:42:28 +0100

    installed Xam.Plugin.Media on android

--- Veganko/Veganko.Android/MainActivity.cs
+++ Veganko/Veganko.Android/MainActivity.cs
@@ -9,6 +9,8 @@ using Android.OS;
 using ImageCircle.Forms.Plugin.Droid;
 using ZXing.Mobile;
 
+[assembly: UsesFeature("android.hardware.camera", Required = false)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = false)]
 namespace Veganko.Droid
 {
     [Activity(Label = "Veganko", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -26,12 +28,14 @@ namespace Veganko.Droid
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
             // Initialize the scanner first so it can track the current context
             MobileBarcodeScanner.Initialize(this.Application);
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Activity = this;
 
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
             global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
