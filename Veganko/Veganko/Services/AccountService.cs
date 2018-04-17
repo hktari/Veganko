86dae6708f665c9e91c24c0a768403edd68e1418
Boštjan Kamnik <bostjan.kamnik@gorenje.com>
2018-04-18 00:01:08 +0200

    sets AccountService User property settter to private

--- Veganko/Veganko/Services/AccountService.cs
+++ Veganko/Veganko/Services/AccountService.cs
@@ -11,7 +11,7 @@ namespace Veganko.Services
 {
     class AccountService : IAccountService
     {
-        public User User { get; set; }
+        public User User { get; private set; }
 
         public bool CreateAccount(string username, string password, string profileImage)
         {
