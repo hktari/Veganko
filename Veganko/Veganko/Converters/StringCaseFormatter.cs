9809839378cd4684e0511ebb35f5740ee7d136e6
Boštjan Kamnik <bkamnik1995@gmail.com>
2019-03-09 11:30:47 +0100

    product title is upper case

---
+++ Veganko/Veganko/Converters/StringCaseFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Veganko.Converters
+{
+    public class StringCaseFormatter : IValueConverter
+    {
+        public bool ToUpperCase { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return ToUpperCase ? str.ToUpper() : str.ToLower();
+            }
+
+            throw new Exception("Invalid value type!");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
