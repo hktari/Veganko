ca2e58a0217eb48305bb530755a3d6d033ee9852
Boštjan Kamnik <bkamnik1995@gmail.com>
2019-03-14 01:19:46 +0100

    tints toolbar

---
+++ Veganko/Veganko.Android/Renderers/CustomNavigationPageRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(Veganko.Droid.Renderers.CustomNavigationPageRenderer))]
+namespace Veganko.Droid.Renderers
+{
+    public class CustomNavigationPageRenderer : NavigationPageRenderer
+    {
+        public CustomNavigationPageRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<NavigationPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                e.NewElement.BarBackgroundColor = Color.FromHex("#5eaa00");
+            }
+        }
+    }
+}
