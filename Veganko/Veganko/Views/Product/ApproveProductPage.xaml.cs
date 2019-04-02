ed4760c1e40c132e1cbd833d6f19ebe56cd99631
Boštjan Kamnik <bkamnik1995@gmail.com>
2019-04-03 01:10:11 +0200

    reworks approve product page

--- Veganko/Veganko/Views/Product/ApproveProductPage.xaml.cs
+++ Veganko/Veganko/Views/Product/ApproveProductPage.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Veganko.Models;
+using Veganko.Other;
 using Veganko.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinImageUploader;
+using Plugin.Media;
 
 namespace Veganko.Views.Product
 {
@@ -18,7 +23,59 @@ namespace Veganko.Views.Product
 		{
 			InitializeComponent ();
             BindingContext = this.vm = vm;
-		}
+            TypePicker.SelectedIndexChanged += TypePickerSelectedIndexChanged;
+            CameraButton.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                vm.Product.ImageName = await ImageManager.UploadImage(file.GetStream());
+                await DisplayAlert("Alert", "Successfully uploaded image", "OK");
+                ProductImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            };
+        }
+
+        private void TypePickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = sender as Picker;
+            var type = (ProductType)Enum.Parse(typeof(ProductType), picker.SelectedItem as string, true);
+            SelectableEnumImageView.Source = new ObservableCollection<ProductClassifier>(EnumConfiguration.ClassifierDictionary[type]) ?? new ObservableCollection<ProductClassifier>();
+        }
+
+        async void Scan_Clicked(object sender, EventArgs e)
+        {
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            var result = await scanner.Scan();
+
+            if (result != null)
+            {
+                vm.Product.Barcode = result.Text;
+                await DisplayAlert("Obvestilo", "Skeniranje končano !", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Obvestilo", "Napaka pri skeniranju !", "OK");
+            }
+        }
 
         private async void OnApproveProductClicked(object sender, EventArgs arg)
         {
@@ -27,6 +84,8 @@ namespace Veganko.Views.Product
             if (result == "Yes")
             {
                 vm.ApproveProductCommand?.Execute(null);
+
+                await Navigation.PopAsync();
             }
         }
 
@@ -37,6 +96,8 @@ namespace Veganko.Views.Product
             if (result == "Yes")
             {
                 vm.DeleteProductCommand?.Execute(null);
+
+                await Navigation.PopAsync();
             }
         }
     }
