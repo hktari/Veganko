772b03086dd7ebf61d4eea7b3372882dbdac6957
hktari <bkamnik1995@gmail.com>
2019-10-05 22:05:22 +0200

    adds implementaiton of mock auth service

---
+++ Veganko/Veganko/Services/Auth/MockAuthService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veganko.Services.Auth
+{
+    public class MockAuthService : IAuthService
+    {
+        public Task<bool> CredentialsExist()
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<string> GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsTokenValid()
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task Login(string email, string password)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Logout()
+        {
+        }
+
+        public Task RefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
