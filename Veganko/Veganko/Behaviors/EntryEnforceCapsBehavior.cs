c3b14e0ff15629b5c9f18caa9801577fb12e13fb
bostjan.kamnik <bostjan.kamnik@gorenje.com>
2019-11-02 10:59:30 +0100

    enforces caps input on name and brand on edit product view

---
+++ Veganko/Veganko/Behaviors/EntryEnforceCapsBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Veganko.Behaviors
+{
+    public class EntryEnforceCapsBehavior : Behavior<Entry>
+    {
+        private Entry entry;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            entry = bindable;
+            bindable.TextChanged += OnTextChanged;
+        }
+        
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnTextChanged;
+            entry = null;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            entry.Text = e.NewTextValue.ToUpperInvariant();
+        }
+    }
+}
