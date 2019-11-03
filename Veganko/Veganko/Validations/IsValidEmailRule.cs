c75523d369888d7c28871caf5b42b6576c8aff1b
bostjan.kamnik <bostjan.kamnik@gorenje.com>
2019-11-03 10:13:29 +0100

    adds email and min length validations rules

---
+++ Veganko/Veganko/Validations/IsValidEmailRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace Veganko.Validations
+{
+    public class IsValidEmailRule : IValidationRule<string>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(string value)
+        {
+            try
+            {
+                _ = new MailAddress(value);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
