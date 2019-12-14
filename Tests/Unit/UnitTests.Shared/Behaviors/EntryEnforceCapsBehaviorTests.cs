c8aad9d1b08c51b0af42a8b503914ecf16f195e9
hktari <bkamnik1995@gmail.com>
2019-12-14 13:20:15 +0100

    fixes null ref ex EntryEnforeCapsBehavior text changed

---
+++ Tests/Unit/UnitTests.Shared/Behaviors/EntryEnforceCapsBehaviorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Veganko.Behaviors;
+using Xamarin.Forms;
+
+namespace UnitTests.Shared.Behaviors
+{
+    [TestClass]
+    public class EntryEnforceCapsBehaviorTests
+    {
+        [TestMethod]
+        public void TestOnTextChanged()
+        {
+            var entryBehav = new EntryEnforceCapsBehavior();
+            Entry entry = new Entry();
+            entry.Behaviors.Add(entryBehav);
+
+            entry.Text = "test";
+
+            Assert.AreEqual("TEST", entry.Text);
+        }
+    }
+}
