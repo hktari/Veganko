06d7f1f5f163417eca6990db275da47247a5fe1a
bostjan.kamnik <bostjan.kamnik@gorenje.com>
2020-01-21 02:00:53 +0100

    adds stores repository

---
+++ Services/VegankoService/Data/Stores/IStoresRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VegankoService.Models;
+using VegankoService.Models.Stores;
+
+namespace VegankoService.Data.Stores
+{
+    public interface IStoresRepository
+    {
+        IEnumerable<Store> GetAll(string productId);
+        Task<Store> Get(string id);
+        Task Create(Store store);
+        Task Update(Store store);
+        Task Delete(Store store);
+    }
+}
---
+++ Services/VegankoService/Data/Stores/StoresRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VegankoService.Models;
+using VegankoService.Models.Stores;
+
+namespace VegankoService.Data.Stores
+{
+    public class StoresRepository : IStoresRepository
+    {
+        private readonly VegankoContext context;
+
+        public StoresRepository(VegankoContext context)
+        {
+            this.context = context;
+        }
+
+        public Task Create(Store store)
+        {
+            context.Add(store);
+            return context.SaveChangesAsync();
+        }
+
+        public Task Delete(Store store)
+        {
+            context.Store.Remove(store);
+            return context.SaveChangesAsync();
+        }
+
+        public Task<Store> Get(string id)
+        {
+            return context.Store.FindAsync(id);
+        }
+
+        public IEnumerable<Store> GetAll(string storeId)
+        {
+            return context.Store.Where(store => store.ProductId == storeId);
+        }
+
+        public Task Update(Store store)
+        {
+            context.Store.Update(store);
+            return context.SaveChangesAsync();
+        }
+    }
+}
