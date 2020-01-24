33e10a77c9f85fcdb04b04ed338925b310fb263c
bostjan.kamnik <bostjan.kamnik@gorenje.com>
2020-01-24 01:51:36 +0100

    adds store migration and makes all services scoped since db context needs to be scoped

--- Services/VegankoService/Migrations/20200123235401_Add-Store.Designer.cs
+++ Services/VegankoService/Migrations/20200123235401_Add-Store.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VegankoService.Data;
 
 namespace VegankoService.Migrations
 {
     [DbContext(typeof(VegankoContext))]
-    partial class VegankoContextModelSnapshot : ModelSnapshot
+    [Migration("20200123235401_Add-Store")]
+    partial class AddStore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
