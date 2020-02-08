08c020cad01a61007ef5015bae55b47f7876e6f4
bostjan.kamnik <bostjan.kamnik@gorenje.com>
2020-02-09 00:21:16 +0100

    adapts entry effect for iOS

---
+++ Veganko/Veganko.iOS/Effects/CustomEntryEffect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(Veganko.iOS.Effects.CustomEntryEffect), "CustomEntryEffect")]
+namespace Veganko.iOS.Effects
+{
+    public class CustomEntryEffect : PlatformEffect
+    {
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if(Control?.Layer != null)
+            {
+                Control.Layer.BorderColor = Color.White.ToCGColor();
+                // Thickness of the Border Width  
+                Control.Layer.BorderWidth = 1;
+                Control.ClipsToBounds = true;
+                Control.TintColor = Color.FromHex("#448e00").ToUIColor();
+            }
+        }
+        protected override void OnAttached()
+        {
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
