591a3006ff0d81ddc47d8cedc9948bc61728c9af
bostjan.kamnik <bostjan.kamnik@gorenje.com>
2020-03-17 08:49:04 +0100

    adds mock email service

---
+++ Services/VegankoService/Services/MockEmailService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VegankoService.Services
+{
+    public class MockEmailService : IEmailService
+    {
+        public static string LastSentToEmail { get; private set; }
+
+        public bool IsEmailProviderSupported(string email) => true;
+
+        public Task SendEmail(string email, string subject, string message)
+        {
+            LastSentToEmail = email;
+            return Task.CompletedTask;
+        }
+    }
+}
